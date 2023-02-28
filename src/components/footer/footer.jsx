522f6054863bc4577baab7cb841947e670ee4793
Asilbek2002 <school2924@gmail.com>
2023-02-28 05:25:05 +0500

    syte: fix video

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,17 +1,24 @@
 import cls from "./footer.module.scss";
 import Logo from "../../assets/images/Frame.svg";
 import { BsInstagram, BsLinkedin, BsTwitter, BsYoutube } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 function Footer() {
+  const navigate = useNavigate();
   return (
     <div className={cls.wrapper}>
       <div className={cls.container}>
         <div className={cls.left}>
-          <img className={cls.logo} src={Logo} alt="" />
+          <img
+            onClick={() => navigate("/")}
+            className={cls.logo}
+            src={Logo}
+            alt=""
+          />
           <div className={cls.links}>
-            <BsYoutube   size={25} />
-            <BsTwitter   size={25} />
-            <BsLinkedin  size={25} />
+            <BsYoutube size={25} />
+            <BsTwitter size={25} />
+            <BsLinkedin size={25} />
             <BsInstagram size={25} />
           </div>
         </div>
--- src/pages/main/components/moreAbout/moreAbout.jsx
+++ src/pages/main/components/moreAbout/moreAbout.jsx
@@ -4,7 +4,12 @@ function MoreAbout() {
   return (
     <div className={cls.wrapper}>
       <div className={cls.bg} onMouseLeave={() => console.log("chiqtiiiii")}>
-        <video autoPlay muted loop src="/public/trucks-37379.mp4"></video>
+        <video
+          autoPlay
+          muted
+          loop
+          src="https://cdn.pixabay.com/vimeo/782374009/road-143479.mp4?width=1920&expiry=1678235131&hash=063a4a1c1ea688b23ce6e1e54b2d3d631c2c8caf"
+        ></video>
       </div>
       <h3
         onMouseOver={(e) => {
